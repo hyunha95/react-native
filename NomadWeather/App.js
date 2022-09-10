79e73afb8a9d3925bf24f7ae3b8a4277b15fb699
nohyunha95 <nohyunha95@gmail.com>
2022-09-10 15:54:49 +0900

    날씨 어플 완성

--- NomadWeather/App.js
+++ NomadWeather/App.js
@@ -1,18 +1,104 @@
+import * as Location from "expo-location";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Dimensions,
+  Text,
+  View,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
+import { Fontisto } from "@expo/vector-icons";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+
+const API_KEY = "c4df86adda4a8c9682507993770fa28f";
+
+const icons = {
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Drizzle: "rain",
+  Thunderstorm: "lightning",
+};
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+  const { date, setDate } = useState(new Date());
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    // console.log(permission);
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    // console.log(location);
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=c4df86adda4a8c9682507993770fa28f&units=metric`
+    );
+    const json = await response.json();
+    setDays(json.daily);
+    console.log(json);
+  };
+  useEffect(() => {
+    getWeather();
+  }, []);
   return (
     <View style={styles.constainer}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
-      <View style={styles.weather}>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-      </View>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator
+              color="white"
+              style={{ marginTop: 10 }}
+              size="large"
+            />
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View style={styles.day}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  width: "100%",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Text style={styles.temp}>
+                  {parseFloat(day.temp.day).toFixed(1)}
+                </Text>
+                <Fontisto
+                  name={icons[day.weather[0].main]}
+                  size={68}
+                  color="black"
+                />
+              </View>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
+      </ScrollView>
     </View>
   );
 }
@@ -31,20 +117,21 @@ const styles = StyleSheet.create({
     fontSize: 68,
     fontWeight: "500",
   },
-  weather: {
-    flex: 3,
-  },
+  weather: {},
   day: {
-    flex: 1,
-    alignItems: "center",
-    backgroundColor: "teal",
+    width: SCREEN_WIDTH,
+    // alignItems: "center",
   },
   temp: {
     marginTop: 50,
-    fontSize: 178,
+    fontWeight: "600",
+    fontSize: 120,
   },
   description: {
     marginTop: -30,
-    fontSize: 60,
+    fontSize: 40,
+  },
+  tinyText: {
+    fontSize: 20,
   },
 });

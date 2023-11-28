ab671e73fce2beafd98c0deb277762e7e26ed203
nohyunha95 <nohyunha95@gmail.com>
2023-11-28 23:31:57 +0900

    react native

--- NomadWeather/App.js
+++ NomadWeather/App.js
@@ -1,112 +1,60 @@
+import { View, Text, StyleSheet } from "react-native";
 import * as Location from "expo-location";
-import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
-import {
-  StyleSheet,
-  Dimensions,
-  Text,
-  View,
-  ScrollView,
-  ActivityIndicator,
-} from "react-native";
-import { Fontisto } from "@expo/vector-icons";
 
-const { width: SCREEN_WIDTH } = Dimensions.get("window");
-
-const API_KEY = "c4df86adda4a8c9682507993770fa28f";
-
-const icons = {
-  Clouds: "cloudy",
-  Clear: "day-sunny",
-  Atmosphere: "cloudy-gusts",
-  Snow: "snow",
-  Drizzle: "rain",
-  Thunderstorm: "lightning",
-};
+const API_KEY = "5b531a13512c2f29d839f8eb246899ce";
 
 export default function App() {
   const [city, setCity] = useState("Loading...");
   const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
-  const { date, setDate } = useState(new Date());
+
   const getWeather = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
-    // console.log(permission);
     if (!granted) {
       setOk(false);
     }
+
     const {
       coords: { latitude, longitude },
     } = await Location.getCurrentPositionAsync({ accuracy: 5 });
-    // console.log(location);
     const location = await Location.reverseGeocodeAsync(
       { latitude, longitude },
       { useGoogleMaps: false }
     );
+
     setCity(location[0].city);
+
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=c4df86adda4a8c9682507993770fa28f&units=metric`
+      `https://api.open-meteo.com/v1/forecast?latitude=${longitude}&longitude=${latitude}`
     );
     const json = await response.json();
-    setDays(json.daily);
     console.log(json);
   };
+
   useEffect(() => {
     getWeather();
   }, []);
+
   return (
-    <View style={styles.constainer}>
+    <View style={styles.container}>
       <View style={styles.city}>
         <Text style={styles.cityName}>{city}</Text>
       </View>
-      <ScrollView
-        horizontal
-        pagingEnabled
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={styles.weather}
-      >
-        {days.length === 0 ? (
-          <View style={styles.day}>
-            <ActivityIndicator
-              color="white"
-              style={{ marginTop: 10 }}
-              size="large"
-            />
-          </View>
-        ) : (
-          days.map((day, index) => (
-            <View style={styles.day}>
-              <View
-                style={{
-                  flexDirection: "row",
-                  alignItems: "center",
-                  width: "100%",
-                  justifyContent: "space-between",
-                }}
-              >
-                <Text style={styles.temp}>
-                  {parseFloat(day.temp.day).toFixed(1)}
-                </Text>
-                <Fontisto
-                  name={icons[day.weather[0].main]}
-                  size={68}
-                  color="black"
-                />
-              </View>
-              <Text style={styles.description}>{day.weather[0].main}</Text>
-              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
-            </View>
-          ))
-        )}
-      </ScrollView>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  constainer: {
+  container: {
     flex: 1,
-    backgroundColor: "#ffd400",
+    backgroundColor: "#fffaaa",
   },
   city: {
     flex: 1.2,
@@ -117,21 +65,19 @@ const styles = StyleSheet.create({
     fontSize: 68,
     fontWeight: "500",
   },
-  weather: {},
+  weather: {
+    flex: 3,
+  },
   day: {
-    width: SCREEN_WIDTH,
-    // alignItems: "center",
+    flex: 1,
+    alignItems: "center",
   },
   temp: {
     marginTop: 50,
-    fontWeight: "600",
-    fontSize: 120,
+    fontSize: 178,
   },
   description: {
     marginTop: -30,
-    fontSize: 40,
-  },
-  tinyText: {
-    fontSize: 20,
+    fontSize: 60,
   },
 });
---
+++ RNCourse/components/GoalItem.js
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View, Pressable } from "react-native";
+
+function GoalItem(props) {
+  return (
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#dddddd" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
+        <Text style={styles.goalText}>{props.text}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+export default GoalItem;
+
+const styles = StyleSheet.create({
+  goalItem: {
+    margin: 10,
+    padding: 10,
+    backgroundColor: "#5e0acc",
+    borderRadius: 10,
+  },
+  pressedItem: {
+    opacity: 0.5,
+  },
+  goalText: {
+    color: "white",
+  },
+});
